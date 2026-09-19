225fd9097dfd1f2af28155154dc20925f35683ff
B-Jogana <193931131+B-Jogana@users.noreply.github.com>
2026-09-19 18:58:24 +0100

    add async file

---
+++ src/Js-Practice/Async.js
@@ -0,0 +1,49 @@
+//promise
+const promise = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Login successful");
+  }, 1000);
+});
+
+promise
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//Asyn/await 
+async function getData() {
+   try {
+    const result = await Promise.resolve("Data received!");
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  } Promise.resolve("Data received");
+
+  
+}
+
+getData();
+//console.log(promise);
+
+//Async promise
+/* fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then((res)=> res.json())
+.then((data)=>console.log(data))
+console.log("jonas");
+ */
+
+async function getUser() {
+  try{
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await response.json();
+    console.log(data);
+  }catch(error){
+    console.log("Error", error)
+  }
+}
+
+getUser();
